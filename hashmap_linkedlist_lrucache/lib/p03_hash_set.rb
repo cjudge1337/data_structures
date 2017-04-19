6595b934d3244429afd7c736b426262c1add4aaf
Carson Judge <cjudge1337@gmail.com>
2017-04-18 19:37:16 -0700

    Finish HashSet

--- hashmap_linkedlist_lrucache/lib/p03_hash_set.rb
+++ hashmap_linkedlist_lrucache/lib/p03_hash_set.rb
@@ -1,4 +1,5 @@
 require_relative 'p02_hashing'
+require 'byebug'
 
 class HashSet
   attr_reader :count
@@ -9,18 +10,34 @@ class HashSet
   end
 
   def insert(key)
+    hash_num = key.hash
+    return if include?(key)
+    resize! if @count >= @store.length
+    self[hash_num] << hash_num
+    @count += 1
+    hash_num
   end
 
   def include?(key)
+    hash_num = key.hash
+    return true if self[hash_num].include?(hash_num)
+    false
   end
 
   def remove(key)
+    # debugger
+    hash_num = key.hash
+    return if include?(key) == false
+    self[hash_num].delete(hash_num)
+    @count -= 1
+    hash_num
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -28,5 +45,14 @@ class HashSet
   end
 
   def resize!
+    new_store = Array.new(num_buckets * 2) { Array.new }
+
+    @store.each do |bucket|
+      bucket.each do |int|
+        new_store[int % (num_buckets * 2)] << int
+      end
+    end
+
+    @store = new_store
   end
 end
